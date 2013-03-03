3404a15dad8567ea98a9c3e7efbd68dd97e63fe1
NoUsername <paul@paukl.at>
2013-03-03 16:11:02 +0100

    show events/second in nodejs page, touch support (mobile & laptop), fix websocket url

--- static/web/public/js/main.js
+++ static/web/public/js/main.js
@@ -4,9 +4,12 @@
 	window.app = app;
 	app.comm = app.comm || {};
 
+	app.isTouch = false;
+
+	var eventCount = 0; // used to compute events/second
 	var gridSize = 2500;
 	var emptyMarker = "#000";
-	var userMarker = get_random_color(); //String.fromCharCode(65 + Math.floor(Math.random()*20));
+	var userMarker = get_random_color();
     
     function get_random_color() {
         var letters = '0123456789ABCDEF'.split('');
@@ -19,6 +22,7 @@
 
 	function DivGridViewModel() {
 		var self = this;
+		self.eventsPerSecond = ko.observable(0);
 		self.divs = Array();
 
 		for (var i=0; i<gridSize; i++) {
@@ -30,10 +34,28 @@
 		return parseInt($(obj).attr('data-idx'));
 	}
 
+	app.recalculateEventsPerSecond = function() {
+		var seconds = 5;
+		app.viewModel.eventsPerSecond(eventCount / seconds);
+		eventCount = 0;
+		setTimeout(app.recalculateEventsPerSecond, seconds*1000);
+	}
+
+	app.selectionChange = function(element, activated) {
+		eventCount++;
+		var idx = getIndex(element);
+		var marker = activated ? userMarker : emptyMarker;
+		app.viewModel.divs[idx](marker);
+		app.comm.send(idx, marker);
+	}
+
 
 	app.start = function() {
 		var container = $('#container');
 		for (var i=0; i<gridSize; i++) {
+			if (i>0 && i % 50 == 0) {
+				container.append('<div class="breaker">&nbsp;</div>');
+			}
 			container.append('<div data-bind="style: { backgroundColor: divs['+i+']() }" data-idx="'+i+'">&nbsp;</div>');
 		}
         
@@ -42,31 +64,62 @@
 		app.viewModel = new DivGridViewModel();
 		ko.applyBindings(app.viewModel);
 
+		app.lastTouched = null;
 		$(document).on('mouseenter', '#container>div', function(e) {
-			//console.log("mouse entered");
-			var idx = getIndex(e.target);
-			app.viewModel.divs[idx](userMarker);
-			app.comm.send(idx, userMarker);
+			app.selectionChange(e.target, true);
+			if (app.lastTouched != null) {
+				app.selectionChange(app.lastTouched, false);
+			}
+			app.lastTouched = e.target;
+		});
+		
+		// disable scrolling here
+		$(document).on('touchstart', '#container>div', function(e) { return false; });
+		$(document).on('touchmove', '#container>div', function(e) {
+			app.touch = e;
+			var posx = e.pageX, posy = e.pageY;
+			if (posx === undefined) { // different apis
+				posx = e.originalEvent.touches[0].pageX;
+				posy = e.originalEvent.touches[0].pageY;
+			}
+			var elem = document.elementFromPoint(posx, posy);
+			if (app.lastTouched != elem) {
+				// changed element
+				if (app.lastTouched != null) {
+					app.selectionChange(app.lastTouched, false);
+				}
+				app.selectionChange(elem, true);
+			}
+			app.lastTouched = elem;
+			e.preventDefault();
+			e.stopPropagation();
+			return false;
 		});
 
-		$(document).on('mouseout', '#container>div', function(e) {
-			//console.log("mouse left");
-			var idx = getIndex(e.target);
-			app.viewModel.divs[idx](emptyMarker);
-			app.comm.send(idx, emptyMarker);
-		});
-
+		// start automatic recalc
+		app.recalculateEventsPerSecond();
 		app.comm.setup();
+
+		try {
+		   document.createEvent("TouchEvent");
+		   app.isTouch = true;
+		   // quick, hacky debug console for mobile devices
+		   // console.log = function(data) { $('body').prepend("<div>" + data + "</div>"); };
+		}
+		catch (e) {}
 	};
 
+	app.comm.connected = false;
+
 	app.comm.setup = function() {
-		var wsUrl = "ws://localhost:8080/";
+		var wsUrl = "ws://"+document.location.hostname+":"+document.location.port+"/";
 		console.log("trying to establish websocket connection with " + wsUrl);
 		var socket = new WebSocket(wsUrl);  
 
 		app.comm.socket = socket;
 
 		socket.onopen = function() {
+			app.comm.connected = true;
 			console.log("connected");
 		};
 
@@ -79,16 +132,18 @@
                 console.log("not a number: " + msg);
                 return;
             }
+            eventCount++;
             app.viewModel.divs[idx](value);
 		};
 
 		socket.onclose = function(){
+			app.comm.connected = false;
 			console.log("ws conn closed!");
 		};
 	};
 
 	app.comm.send = function(idx, val) {
-		if (app.comm.socket) {
+		if (app.comm.connected) {
 			app.comm.socket.send(idx+";"+val);
 		}
 	};
